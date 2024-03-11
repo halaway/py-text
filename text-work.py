da3fbd982c56c79a3b6b0b04ec6fed821c7defd7
halaway <31904474+halaway@users.noreply.github.com>
2024-03-11 12:28:06 -0400

    developing summary model class

--- text-work.py
+++ text-work.py
@@ -4,7 +4,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 
+
+import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
@@ -47,12 +50,62 @@ class TextSummary:
         self.tokenized_entries = self.text_data
 
 
+    ##
+    # 
+    # Creating IDS Using Example Texts
+    # #
+    def create_ids(self):
+
+        # Converting Strings to Numerical Representation
+        example_texts = ['abcdefg', 'xyz']
+        chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+
+        self._chars = chars
+
+
 
 
 def main():
 
 
-    print("gee")
+    # Downloading Example Shakespeare Text
+    path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+    # Reading File from Path
+    text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+    
+    # First 250 characters
+    # print(text[:250])
+
+    # Unique Characters
+    vocab = sorted(set(text))
+
+    # Converting Strings to numerical Representations
+    example_texts = ['abcdefg', 'xyz']
+    chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+
+    # Creating IDs
+    ids_from_chars = tf.keras.layers.StringLookup(\
+        vocabulary=list(vocab), mask_token=None)
+
+    chars_from_ids = tf.keras.layers.StringLookup(
+    vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+
+    ids = ids_from_chars(chars)
+
+    chars = chars_from_ids(ids)
+
+    l = tf.strings.reduce_join(chars, axis=-1).numpy()
+
+    print(l)
+
+   # print(chars)
+
+
+
+
+    #print(ids)
+    
 
     return 0;
 
